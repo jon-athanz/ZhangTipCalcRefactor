c52905ceb4508c670ec4c0d7750d0b894b1770e2
Jonathan <jonathanz28@nycstudents.net>
2022-10-07 09:29:57 -0400

    ZhangTipCalculatorRefactor

--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -1,2 +1,43 @@
-package PACKAGE_NAME;public class TipCalculator {
+public class TipCalculator {
+    private int numPeople;
+    private int tipPercentage;
+    private double totalBillBeforeTip;
+
+    public TipCalculator(int people, int percent) {
+        numPeople = people;
+        tipPercentage = percent;
+        totalBillBeforeTip = 0;
+    }
+
+    public double getTotalBillBeforeTip() {
+        return totalBillBeforeTip;
+    }
+
+    public int getTipPercentage() {
+        return tipPercentage;
+    }
+
+    public void addMeal(double cost) {
+        totalBillBeforeTip += cost;
+    }
+
+    public double tipAmount() {
+        return (tipPercentage / 100.0) * totalBillBeforeTip;
+    }
+
+    public double totalBill() {
+        return tipAmount() + totalBillBeforeTip;
+    }
+
+    public double perPersonCostBeforeTip() {
+        return totalBillBeforeTip / numPeople;
+    }
+
+    public double perPersonTipAmount() {
+        return tipAmount() / numPeople;
+    }
+
+    public double perPersonTotalCost() {
+        return totalBill() / numPeople;
+    }
 }
--- src/TipCalculatorRunner.java
+++ src/TipCalculatorRunner.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class TipCalculatorRunner {
+import java.util.Scanner;
+import java.text.DecimalFormat;
+
+public class TipCalculatorRunner {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        DecimalFormat df = new DecimalFormat("#.00");
+
+        int people;
+        int percent;
+
+        System.out.println("Welcome to the tip calculator!");
+        System.out.println("How many people are in your group?");
+        people = scan.nextInt();
+        System.out.println("What's the tip percentage? (0 - 100) :");
+        percent = scan.nextInt();
+
+        TipCalculator calc = new TipCalculator(people, percent);
+
+        for (double cost = 0; scan.nextDouble() != -1; cost += scan.nextDouble()) {
+            System.out.println("Enter a cost in dollars and cents, e.g. 12.50 (-1 to end) : ");
+            calc.addMeal(cost);
+        }
+
+    }
 }
